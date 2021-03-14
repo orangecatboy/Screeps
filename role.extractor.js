68dfc115386e008583d8a99222afd6d3f2c3e394
orangecatboy <45246428+orangecatboy@users.noreply.github.com>
2021-03-14 10:55:38 -0700

    Create role.extractor.js

---
+++ role.extractor.js
@@ -0,0 +1,41 @@
+var roleExtractor = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        
+        if (creep.memory.extracting && creep.store.getCapacity == _.sum(creep.store)) {
+            creep.memory.extracting = false;
+        }
+        if (!creep.memory.extracting && 0 == _.sum(creep.store)) {
+            creep.memory.extracting = true;
+        } 
+        
+        if (creep.memory.extracting) {
+            var target;
+        
+            if (creep.memory.depositId) {
+                target = Game.getObjectById(creep.memory.depositId);
+            } else {
+                var targets = creep.room.find(FIND_MINERALS);
+                target = targets[0];
+                creep.memory.depositId = target.id;
+                creep.memory.mineralType = target.mineralType;
+            }
+            if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        } else {
+            if (creep.room.terminal) {
+                if (creep.transfer(creep.room.terminal, creep.memory.mineralType) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.terminal);
+                }
+            } else if (creep.room.storage) {
+                if (creep.transfer(creep.room.storage, creep.memory.mineralType) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.storage);
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleExtractor;
