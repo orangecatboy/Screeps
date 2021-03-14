60820a555b4b2460be23912509c353d1f2b4fc15
orangecatboy <45246428+orangecatboy@users.noreply.github.com>
2021-03-14 10:52:54 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -7,8 +7,10 @@ var roleRepairer = require('role.repairer');
 var roleWallRepairer = require('role.wallRepairer');
 var roleTowerManager = require('role.towerManager');
 var roleMiner = require('role.miner');
-
-global.HOME = 'W37N31';
+var roleAttacker = require('role.attacker');
+var roleLorry = require('role.lorry');
+var roleClaimer = require('role.claimer');
+var roleExtractor = require('role.extractor');
 
 module.exports.loop = function () {
     // check for memory entries of died creeps by iterating over Memory.creeps
@@ -25,13 +27,15 @@ for (let spawnName in Game.spawns) {
     let spawn = Game.spawns[spawnName];
     let creepsInRoom = spawn.room.find(FIND_MY_CREEPS);
     myTowers.play();
+
         var minimumNumberOfHarvesters = 3;
+        var minimumNumberOfAttackers = 0;
         var minimumNumberOfUpgraders = 3;
         var minimumNumberOfBuilders = 0;
         var minimumNumberOfRepairers = 2;
         var minimumNumberOfWallRepairers = 2;
         var minimumNumberOfTowerManagers = 2;
-        var minimumNumberOfSupportBuilders = 1;
+        var minimumNumberOfSupportBuilders = 3;
         var minimumNumberOfSupportUpgraders = 1;
         // count the number of creeps alive for each role in this room
         // _.sum will count the number of properties in Game.creeps filtered by the
@@ -41,29 +45,65 @@ for (let spawnName in Game.spawns) {
         var numberOfBuildersInW37N31 = _.sum(creepsInRoom, (c) => c.memory.role == 'builder');
         var numberOfRepairersInW37N31 = _.sum(creepsInRoom, (c) => c.memory.role == 'repairer');
         var numberOfWallRepairersInW37N31 = _.sum(creepsInRoom, (c) => c.memory.role == 'wallRepairer');
+        var numberOfMiners = _.sum(creepsInRoom, (c) => c.memory.role == 'miner');
+        var numberOfAttackers = _.sum(Game.creeps, (c) => c.memory.role == 'attacker');
+        var numberOfLorries = _.sum(creepsInRoom, (c) => c.memory.role == 'lorry');
         var numberOfTowerManagersInW37N31 = _.sum(creepsInRoom, (c) => c.memory.role == 'towerManager');
-        var name = undefined;
-        var bodyparts = [WORK,WORK,CARRY,CARRY,MOVE];
-
+        var numberOfSupportBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.target == 'W37N32');
+        var numberOfExtractors = _.sum(creepsInRoom, (c) => c.memory.role == 'extractor');
         
+        var name = undefined;
 
-        // if not enough harvesters
+        var bodyparts = [WORK,WORK,CARRY,CARRY,MOVE,MOVE];
+
+
+       // if no harvesters are left AND either no miners or no lorries are left
+        //  create a backup creep
+        if (numberOfHarvestersInW37N31 == 0 && (numberOfMiners == 0 || numberOfLorries == 0)) {
+            // if there are still miners left
+            if (numberOfMiners > 0) {
+                // create a lorry
+                name = spawn.createCreep([WORK,CARRY,MOVE], undefined, { role: 'lorry', working: false});
+            }
+            // if there is no miner left
+            else {
+                // create a harvester because it can work on its own
+                name = spawn.createCreep([WORK,CARRY,MOVE], undefined, { role: 'harvester', working: false });
+            }
+        }
+        // if no backup creep is required
+        else {
+            // check if all sources have miners
+            let sources = spawn.room.find(FIND_SOURCES);
+            // iterate over all sources
+            for (let source of sources) {
+                // if the source has no miner
+                if (!_.some(creepsInRoom, c => c.memory.role == 'miner' && c.memory.sourceId == source.id)) {
+                    // check whether or not the source has a container
+                    let containers = source.pos.findInRange(FIND_STRUCTURES, 1, {
+                        filter: s => s.structureType == STRUCTURE_CONTAINER
+                    });
+                    // if there is a container next to the source
+                    if (containers.length > 0) {
+                        // spawn a miner
+                        name = spawn.createCreep([WORK, WORK, WORK, WORK, MOVE], undefined, { role: 'miner', sourceId: source.id });
+                    }
+                }
+            }
+        }
         if (numberOfHarvestersInW37N31 < spawn.memory.minHarvesters) {
             // try to spawn one
         name = spawn.createCreep(bodyparts, undefined,
-            { role: 'harvester', working: false});
-            // if spawning failed and we have no harvesters left
-            if (name == ERR_NOT_ENOUGH_ENERGY && numberOfHarvestersInW37N31 == 0) {
-                // spawn one with what is available
-        name = spawn.createCreep([WORK,CARRY,MOVE], undefined,
-            { role: 'harvester', working: false});            }
-        }
-        // if there is a claim order defined
+            { role: 'harvester', working: false});        }
         // if not enough upgraders
         else if (numberOfUpgradersInW37N31 < spawn.memory.minUpgraders) {
             // try to spawn one
         name = spawn.createCreep(bodyparts, undefined,
             { role: 'upgrader', working: false});        }
+        
+        else if (numberOfLorries < spawn.memory.minLorries){
+        name = spawn.createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'lorry', working: false});
+        }
 //        else if (numberOfSupportUpgraders < minimumNumberOfSupportUpgraders) {
             // try to spawn one
 //        name = spawn.createCreep(bodyparts, undefined,
@@ -73,57 +113,68 @@ for (let spawnName in Game.spawns) {
             // try to spawn one
         name = spawn.createCreep(bodyparts, undefined,
             { role: 'repairer', working: false});        }
-            // if not enough builders
-        else if (numberOfBuildersInW37N31 < spawn.memory.minBuilders) {
-            // try to spawn one
-        name = spawn.createCreep(bodyparts, undefined,
-            { role: 'builder', working: false});        }
-//        else if (numberOfSupportBuilders < minimumNumberOfSupportBuilders) {
-            // try to spawn one
-//        name = spawn.createCreep([WORK,WORK,CARRY,CARRY,MOVE], undefined,
-//            { role: 'builder', working: false, target: 'W37N32', room: 'W37N31'});        }            // if not enough wallRepairers
         else if (numberOfWallRepairersInW37N31 < spawn.memory.minWallRepairers) {
             // try to spawn one
         name = spawn.createCreep(bodyparts, undefined,
             { role: 'wallRepairer', working: false});        }
+            // if not enough builders
+        else if (numberOfBuildersInW37N31 < spawn.memory.minBuilders) {
+            // try to spawn one
+        name = spawn.createCreep(bodyparts, undefined,
+            { role: 'builder', working: false, target: spawn.memory.room});        }
+        //  else if (numberOfSupportBuilders < minimumNumberOfSupportBuilders) {
+        //      // try to spawn one
+        //  name = spawn.createCreep(bodyparts, undefined,
+        //     { role: 'builder', working: false, target: 'W37N32'});        }            // if not enough wallRepairers
         else if (numberOfTowerManagersInW37N31 < spawn.memory.minTowerManagers) {
             // try to spawn one
         name = spawn.createCreep(bodyparts, undefined,
             { role: 'towerManager'});        }
-            // if not enough longDistanceHarvesters for W54S78
-        else {
-            // else try to spawn a builder
+        else if (numberOfExtractors < spawn.memory.minExtractors){
         name = spawn.createCreep(bodyparts, undefined,
-            { role: 'builder', working: false});        }
+            { role: 'extractor'});       
+        }
             
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        else if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        else if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
-        if(creep.memory.role == 'wallRepairer') {
-            roleWallRepairer.run(creep);
-        }
-        if(creep.memory.role == 'repairer') {
+
+        else if(creep.memory.role == 'repairer') {
             roleRepairer.run(creep);
         }
-        if(creep.memory.role == 'towerManager') {
+        else if(creep.memory.role == 'wallRepairer') {
+            roleWallRepairer.run(creep);
+        }
+        else if(creep.memory.role == 'towerManager') {
             roleTowerManager.run(creep);
         }
-        if(creep.memory.role == 'claimer') {
+        else if(creep.memory.role == 'claimer') {
             roleClaimer.run(creep);
         }
-        if(creep.memory.role == 'attacker') {
+        else if(creep.memory.role == 'attacker') {
             roleAttacker.run(creep);
         }  
-        if(creep.memory.role == 'miner') {
+        else if(creep.memory.role == 'miner') {
             roleMiner.run(creep);
+        }
+        // if creep is miner, call miner script
+        else if (creep.memory.role == 'miner') {
+            roleMiner.run(creep);
+        }
+        // if creep is lorry, call miner lorry
+        else if (creep.memory.role == 'lorry') {
+            roleLorry.run(creep);
+        }
+        else if (creep.memory.role == 'extractor'){
+            roleExtractor.run(creep);
         }
     }
 
@@ -136,9 +187,11 @@ for (let spawnName in Game.spawns) {
             console.log("Builders      : " + numberOfBuildersInW37N31);
             console.log("Repairers     : " + numberOfRepairersInW37N31);
             console.log("Wall Repairers : " + numberOfWallRepairersInW37N31);
+            console.log("Miners        : " + numberOfMiners);
+            console.log("Lorries       : " + numberOfLorries);
             console.log("Tower Managers : " + numberOfTowerManagersInW37N31);
         }
-    }
+    };
 
 
         if(Game.cpu.bucket == 10000) {
