60f71ef4d5f5f59b4c01350ae1ab32216784edee
orangecatboy <45246428+orangecatboy@users.noreply.github.com>
2021-03-21 15:27:35 -0700

    Create role.firstWave.js

---
+++ role.firstWave.js
@@ -0,0 +1,23 @@
+var roleAttacker = {
+    // a function to run the logic for this role
+    run: function(creep) {
+        // if in target room
+        creep.moveTo(Game.flags.Attacker);
+        if (creep.room.name !== creep.memory.target) {
+            // find exit to target room
+            creep.say(creep.room.name);
+            var exit = creep.room.findExitTo(creep.memory.target);
+            // move to exit
+            creep.moveTo(creep.pos.findClosestByRange(exit), {visualizePathStyle: {stroke: '#FF3535'}});
+        }
+        else {
+var enemy = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+if(enemy) {
+    if(creep.attack(enemy) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(enemy);
+    }
+}
+        }
+    }
+};
+module.exports = roleAttacker;
