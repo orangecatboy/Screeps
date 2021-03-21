81b383bd7f2cbc5ac9804acd345e071b7178249c
orangecatboy <45246428+orangecatboy@users.noreply.github.com>
2021-03-21 15:27:04 -0700

    Update role.extractor.js

--- role.extractor.js
+++ role.extractor.js
@@ -1,35 +1,33 @@
 var roleExtractor = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function(creep){
         
-        if (creep.memory.extracting && creep.store.getCapacity == _.sum(creep.store)) {
+        if(creep.memory.extracting && creep.store.getCapacity() == _.sum(creep.store)){
             creep.memory.extracting = false;
         }
-        if (!creep.memory.extracting && 0 == _.sum(creep.store)) {
+        else if(!creep.memory.extracting && 0 == _.sum(creep.store)){
             creep.memory.extracting = true;
-        } 
+        }
         
-        if (creep.memory.extracting) {
-            var target;
-        
-            if (creep.memory.depositId) {
+        if(creep.memory.extracting){
+            if(creep.memory.target){
                 target = Game.getObjectById(creep.memory.depositId);
-            } else {
+            }else{
                 var targets = creep.room.find(FIND_MINERALS);
                 target = targets[0];
                 creep.memory.depositId = target.id;
                 creep.memory.mineralType = target.mineralType;
             }
+            
             if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
+                creep.moveTo(target, );
             }
-        } else {
+        }else{
             if (creep.room.terminal) {
                 if (creep.transfer(creep.room.terminal, creep.memory.mineralType) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.terminal);
                 }
-            } else if (creep.room.storage) {
+            }
+            else if (creep.room.storage) {
                 if (creep.transfer(creep.room.storage, creep.memory.mineralType) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.storage);
                 }
